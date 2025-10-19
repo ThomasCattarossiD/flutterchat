e3efab640b4d1be01288b35d374b14385e5bd6ec
ThomasCattarossiD <pro.thomascd@gmail.com>
2025-10-19 15:06:46 +0200

    Ajout des packages nécessaires

---
+++ chat_app/lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kPrimaryColor = Color(0xFF0D47A1);
+const kAccentColor = Color(0xFF42A5F5);
+const kTextColor = Color(0xFF333333);
+const kWhiteColor = Colors.white;
+
+// Text Styles
+const kTitleStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: kTextColor,
+);
+
+const kSubtitleStyle = TextStyle(
+  fontSize: 16,
+  color: kTextColor,
+);
+
+// Paddings
+const double kDefaultPadding = 20.0;
